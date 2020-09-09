65501b83dc118dd77aeb43c6ab130d42eb3f702d
Roshyo <stefdecock68@gmail.com>
2020-09-09 13:33:54 +0200

    Change use order

--- tests/Application/public/index.php
+++ tests/Application/public/index.php
@@ -1,8 +1,8 @@
 <?php
 
 use Symfony\Component\ErrorHandler\Debug;
-use Tests\Setono\SyliusCriteoPlugin\Application\Kernel;
 use Symfony\Component\HttpFoundation\Request;
+use Tests\Setono\SyliusCriteoPlugin\Application\Kernel;
 
 require dirname(__DIR__).'/config/bootstrap.php';
 
